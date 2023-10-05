20a8bc6b38d9d703fc2c80062b4b2d5cf8d9b9be
sakuya <s.hanata.sys23@morijyobi.ac.jp>
2023-10-05 15:05:30 +0900

    Hello Worldを100回出力するプログラム

--- pra2.java
+++ pra2.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello, world!");
+        }
     }
 }
+
